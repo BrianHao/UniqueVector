2b4ca0c1ff827d6967306d180d0218888b06d1c8
Brian Hao <brianhao@gmail.com>
2016-09-23 16:22:42 -0400

    Commit 4
    Started work on classroom.cpp

--- Classroom.cpp
+++ Classroom.cpp
@@ -2,6 +2,62 @@
 #include "UniqueVector.h"
 #include <iostream>
 #include <string>
-
 using namespace std;
 
+//Default constructor
+Classroom::Classroom(){
+    classroomVector = new UniqueVector<string>;
+}
+
+//Constructor with provided initial capacity.
+Classroom::Classroom(unsigned const int initialCapacity){
+    classroomVector = new UniqueVector<string>(initialCapacity);
+}
+
+//Destructor
+Classroom::~Classroom(){
+    if (!classroomVector->empty()) {
+        delete classroomVector;
+    }
+    
+}
+
+//If a student named name is not already on the classroom roster, adds a new student
+//  named name to the classroom roster and returns true; otherwise, returns false.
+bool Classroom::addStudent(const string& name){
+    
+    return 0;
+    
+}
+
+//If a student named name is on the classroom roster, removes the student named name
+//  from the classroom roster and returns true; otherwise, returns false.
+bool Classroom::removeStudent(const string& name){
+    return 0;
+}
+
+//If a student named name is on the classroom roster, returns true; otherwise, returns false.
+bool Classroom::containsStudent(const string& name){
+    return 0;
+}
+
+//Returns a string containing the names of the students in the classroom, separated by commas.
+string Classroom::listAllStudents(){
+    return 0;
+}
+
+//Removes and returns the student whose name comes lexicographically first on the classroom roster.
+string Classroom::removeAlphabeticallyFirst(){
+    return 0;
+}
+
+//Removes and returns the student whose name comes lexicographically last on the classroom roster.
+string Classroom::removeAlphabeticallyLast(){
+    return 0;
+}
+
+//Adds all of the student names on otherClass’ roster onto the roster of the Classroom calling
+//  this function and leaves otherClass unchanged.
+void Classroom::combineClasses(Classroom& otherClass){
+    
+}
--- Classroom.h
+++ Classroom.h
@@ -1,13 +1,25 @@
 #ifndef CLASSROOM_H
 #define CLASSROOM_H
+#include "UniqueVector.h"
 #include <iostream>
 using namespace std;
 
 class Classroom {
     
-    
 public:
+    Classroom();
+    Classroom(unsigned const int initialCapacity);
+    virtual ~Classroom();
+    bool addStudent(const string& name);
+    bool removeStudent(const string& name);
+    bool containsStudent(const string& name);
+    string listAllStudents();
+    string removeAlphabeticallyFirst();
+    string removeAlphabeticallyLast();
+    void combineClasses(Classroom& otherClass);
     
+private:
+    UniqueVector<string>* classroomVector;
 };
 
 #endif
