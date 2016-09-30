5c84788aba3d7eb40864c41d2a4d179888f351ef
Brian Hao <brianhao@gmail.com>
2016-09-29 23:52:52 -0400

    Classroom.h - added one-line implementations, added commented description of functions

--- Classroom.h
+++ Classroom.h
@@ -8,21 +8,18 @@ using namespace std;
 class Classroom {
     
 public:
-    Classroom();
-    Classroom(unsigned const int initialCapacity);
-    virtual ~Classroom();
-    bool addStudent(const string& name);
-    bool removeStudent(const string& name);
-    bool containsStudent(const string& name);
-    string listAllStudents();
-    string removeAlphabeticallyFirst();
-    string removeAlphabeticallyLast();
-    void combineClasses(Classroom& otherClass);
-    unsigned int classroomSize() const;
-    string studentAt(unsigned int pos) const;
+    Classroom() {};
+    virtual ~Classroom() {};
+    bool addStudent(const string& name) { return classroomVector.insert(name); }; //Adds unique student to the end of the roster
+    bool removeStudent(const string& name) { return classroomVector.remove(name); }; //Removes specified student from the roster
+    bool containsStudent(const string& name) { return classroomVector.contains(name); };   //Checks if specified student is in the roster
+    string listAllStudents();   //Returns a list of the roster, separated by commas
+    string removeAlphabeticallyFirst(); //Removes the alphabetically first student
+    string removeAlphabeticallyLast();  //Removes the alphabetically last student
+    void combineClasses(Classroom& otherClass); //Merges a second roster with the current roster
     
 private:
-    UniqueVector<string>* classroomVector;
+    UniqueVector<string> classroomVector;
 };
 
 #endif
